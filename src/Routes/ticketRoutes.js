1598d88874df7ab976c0992c0ad8a6c0de19c446
kalesd11 <surajkales111@gmail.com>
2023-11-28 23:27:38 +0530

    Added confirm ticket

--- src/Routes/ticketRoutes.js
+++ src/Routes/ticketRoutes.js
@@ -67,7 +67,7 @@ router.post("/payment", async (req, res) => {
 });
 
 router.post("/webhook", express.raw({ type: 'application/json' }), async (req, res) => {
-  const payload = req.body.toString();
+  const payload = JSON.stringify(req.body);
   const sig = req.headers["stripe-signature"];
 
   let event;
